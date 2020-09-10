1f67856178434f38b9509549b15c7ed487b919c2
Travis Tincher <travistincher@Traviss-MacBook-Air.local>
2020-09-10 18:12:52 -0500

    added handleChange and handleSubmit to Signup

--- brewery-map-app/src/Components/SignUp/SignUp.js
+++ brewery-map-app/src/Components/SignUp/SignUp.js
@@ -1,16 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SignUp.css";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
 function SignUp() {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+  const [rePassword, setRePassword] = useState({
+    rePassword: "",
+  });
+
+  console.log("user in LogIn", user);
+
+  const handleChange = (e) => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleChangeRePassword = (e) => {
+    setRePassword({
+      ...rePassword,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("user in handleSubmit", user);
+    console.log("rePassword in handleSubmit", rePassword);
+  };
+
   return (
     <div className="SignUp">
       <h1>Sign Up</h1>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Form.Group controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            value={user.email}
+            name="email"
+            onChange={handleChange}
+          />
           <Form.Text id="passwordHelpBlock" muted>
             Your password must be 8-20 characters long, contain letters and
             numbers, and must not contain spaces, special characters, or emoji.
@@ -18,12 +54,24 @@ function SignUp() {
         </Form.Group>
         <Form.Group controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            value={user.password}
+            name="password"
+            onChange={handleChange}
+          />
         </Form.Group>
 
         <Form.Group controlId="formBasicPassword">
           <Form.Label>Re-Enter Password</Form.Label>
-          <Form.Control type="password" placeholder="Re-Enter Password" />
+          <Form.Control
+            type="password"
+            placeholder="Re-Enter Password"
+            value={user.rePassword}
+            name="rePassword"
+            onChange={handleChangeRePassword}
+          />
         </Form.Group>
 
         <Button variant="primary" type="submit">
